def39d0f2fb32aeb211b46f0537612b9d5ff9dd3
piasmajumdar <piasmajumdar@gmail.com>
2026-05-06 15:41:37 +0530

    favicon added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,13 +5,16 @@ import Footer from '@/components/Footer';
 export const metadata = {
   title: 'Local Discovery',
   description: 'AI-Based Local Discovery',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/image/LD_logo.png" type="image/x-icon" />
+
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
