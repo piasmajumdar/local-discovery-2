081ea2e66db74f800da4e50042b46e224595c8d9
piasmajumdar <piasmajumdar@gmail.com>
2026-05-07 02:21:25 +0530

    analytics added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: 'Local Discovery',
