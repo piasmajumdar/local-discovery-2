e42eb753fc8456487e96756397ad1169a67c19fe
piasmajumdar <piasmajumdar@gmail.com>
2026-05-07 02:30:47 +0530

    insights added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Local Discovery',
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
